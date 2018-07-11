9f104cfb566c2304967d1676a1adce00ccb170ef
Robson Cartes <robsoncartes@outlook.com>
2018-07-11 03:01:35 -0300

    Tela de confirmação de pedido.

---
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { PedidoDto } from '../../models/pedido.dto';
+import { CartItem } from '../../models/cart-item';
+import { CartService } from '../../services/domain/cart.service';
+import { ClienteDto } from '../../models/cliente.dto';
+import { EnderecoDto } from '../../models/endereco.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
+
+@IonicPage()
+@Component({
+  selector: 'page-order-confirmation',
+  templateUrl: 'order-confirmation.html',
+})
+export class OrderConfirmationPage {
+
+  pedido: PedidoDto;
+  cartItems: CartItem[];
+  cliente: ClienteDto;
+  endereco: EnderecoDto;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public cartService: CartService,
+    public clienteService: ClienteService) {
+
+    this.pedido = this.navParams.get('pedido');
+  }
+
+  ionViewDidLoad() {
+    this.cartItems = this.cartService.getCart().items;
+    this.clienteService.findById(this.pedido.cliente.id)
+      .subscribe(response => {
+        this.cliente = response as ClienteDto;
+        this.endereco = this.findEndereco(this.pedido.enderecoDeEntrega.id, response['enderecos']);
+      }, error => {
+        this.navCtrl.setRoot('HomePage');
+      })
+  }
+
+  private findEndereco(id: string, list: EnderecoDto[]) : EnderecoDto {
+    let position = list.findIndex(x => x.id == id);
+    return list[position];
+  }
+
+  total(){
+    return this.cartService.total();
+  }
+}
